353db081d86d6b0d4fdb216f6dbc1a85ba0b4c23
20214681 <h.choi2@student.tue.nl>
2023-03-20 16:21:33 +0100

    sharpening techniques notebook

---
+++ DL/Lib/site-packages/plotext/__init__.py
@@ -0,0 +1,7 @@
+"""\nplotext plots directly on terminal"""
+    
+__name__ = "plotext"
+__version__ = "5.2.8"
+
+from ._core import *
+#from .plotext_cli import build_parser # not sure why this line was here
